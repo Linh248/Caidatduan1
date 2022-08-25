4042a3d590c5554edd1d51190ff7ccfabd1eea0a
Linh <trannhutlinh0925@gmail.com>
2022-08-25 13:13:57 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
-// app.get("/", (req, res) => {
-//     res.json({ message: "Welcome to contact book application." });
-// });
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
+
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
